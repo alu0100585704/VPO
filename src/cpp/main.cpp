68de9833481013b4c088c5872ae9e20ecfaa92d1
Juan Carlos "RMPixel <carlos@rmpcorreo.com>
2020-10-30 19:54:32 +0000

    Pruebas

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -6,15 +6,15 @@ int main(int argc, char *argv[])
 {
 
     QCoreApplication::setApplicationName("Grupo Maquinas");
-    QCoreApplication::setOrganizationDomain("SIPC: Reconocimiento Gestos");
-    QCoreApplication::setOrganizationName("SIPC");
+    QCoreApplication::setOrganizationDomain("VPO");
+    QCoreApplication::setOrganizationName("VPO");
 
     QApplication a(argc, argv);
     MainWindow w;
 
 
     //w.setWindowIcon(QIcon(":/Recursos/Images/RMpixel ico 64x64.png"));
-    w.setWindowTitle("SIPC: Reconocimiento Gestos");
+    w.setWindowTitle("VPO: Mi FotoVisión");
     w.show();
     return a.exec();
 }
--- src/include/mainwindow.h
+++ src/include/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/video/background_segm.hpp>
+using namespace  cv;
+using namespace  std;
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +24,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
 };
 
 #endif // MAINWINDOW_H
