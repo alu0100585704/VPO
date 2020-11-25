ae4b029d35f36f449cc8edaeceeb385bbf56e7a4
Juan Carlos "RMPixel <carlos@rmpcorreo.com>
2020-11-25 20:59:22 +0000

    probando con las tildes

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -2,6 +2,7 @@
 #include <QApplication>
 #include <QCoreApplication>
 #include <QTextCodec>
+#include <QObject>
 int main(int argc, char *argv[])
 {
 
@@ -15,7 +16,8 @@ int main(int argc, char *argv[])
 
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8") );
     w.setWindowIcon(QIcon(":/Recursos/Images/RMpixel ico 64x64.png"));
-    w.setWindowTitle("VPO: Mi FotoVision");
+
+    w.setWindowTitle(QObject::tr("VPO: Mi FotoVision"));
     w.show();
     return a.exec();
 }
